0b9329f14db3d147b88088dc34dcdf6eeac65c2c
spel werdz rite <spelwerdzrite@gmail.com>
2026-09-13 21:53:22 -0700

    Fix sizes of several controls
    * A lot of control were edited in a 125% windows scale factor
      environment. However, Visual Studio Forms Designer is DPI unaware, so
      the dimensions that get designed and the dimensions that get written
      end up being different and non-portable. This commit fixes the wrong
      dimensions.
    * Updates Designer.cs files to follow new format.
    * Add resx files to other component objects.
    * Update settings file to latest version.

--- src/Brutario.Win/AutoSaveForm.Designer.cs
+++ src/Brutario.Win/AutoSaveForm.Designer.cs
@@ -71,7 +71,7 @@ partial class AutoSaveForm
         chkAutoSave.CheckState = CheckState.Checked;
         chkAutoSave.Location = new Point(12, 12);
         chkAutoSave.Name = "chkAutoSave";
-        chkAutoSave.Size = new Size(135, 24);
+        chkAutoSave.Size = new Size(109, 19);
         chkAutoSave.TabIndex = 2;
         chkAutoSave.Text = "Auto save every";
         chkAutoSave.UseVisualStyleBackColor = true;
@@ -81,7 +81,7 @@ partial class AutoSaveForm
         // 
         tbxTime.Location = new Point(153, 10);
         tbxTime.Name = "tbxTime";
-        tbxTime.Size = new Size(73, 27);
+        tbxTime.Size = new Size(73, 23);
         tbxTime.TabIndex = 3;
         tbxTime.TextChanged += Time_TextChanged;
         // 
@@ -91,7 +91,7 @@ partial class AutoSaveForm
         cbxUnits.Items.AddRange(new object[] { "seconds", "minutes", "hours" });
         cbxUnits.Location = new Point(232, 10);
         cbxUnits.Name = "cbxUnits";
-        cbxUnits.Size = new Size(151, 28);
+        cbxUnits.Size = new Size(151, 23);
         cbxUnits.TabIndex = 4;
         // 
         // chkPrune
@@ -101,7 +101,7 @@ partial class AutoSaveForm
         chkPrune.CheckState = CheckState.Checked;
         chkPrune.Location = new Point(12, 46);
         chkPrune.Name = "chkPrune";
-        chkPrune.Size = new Size(222, 24);
+        chkPrune.Size = new Size(176, 19);
         chkPrune.TabIndex = 5;
         chkPrune.Text = "Delete auto-saves older than";
         chkPrune.UseVisualStyleBackColor = true;
@@ -111,7 +111,7 @@ partial class AutoSaveForm
         // 
         tbxCutoffTime.Location = new Point(240, 44);
         tbxCutoffTime.Name = "tbxCutoffTime";
-        tbxCutoffTime.Size = new Size(75, 27);
+        tbxCutoffTime.Size = new Size(75, 23);
         tbxCutoffTime.TabIndex = 6;
         tbxCutoffTime.TextChanged += Time_TextChanged;
         // 
@@ -121,7 +121,7 @@ partial class AutoSaveForm
         cbxCutoffUnits.Items.AddRange(new object[] { "minutes", "hours", "days", "weeks" });
         cbxCutoffUnits.Location = new Point(321, 44);
         cbxCutoffUnits.Name = "cbxCutoffUnits";
-        cbxCutoffUnits.Size = new Size(116, 28);
+        cbxCutoffUnits.Size = new Size(116, 23);
         cbxCutoffUnits.TabIndex = 7;
         // 
         // gbxPruneMode
@@ -140,7 +140,7 @@ partial class AutoSaveForm
         rdbProgressive.AutoSize = true;
         rdbProgressive.Location = new Point(6, 56);
         rdbProgressive.Name = "rdbProgressive";
-        rdbProgressive.Size = new Size(388, 24);
+        rdbProgressive.Size = new Size(307, 19);
         rdbProgressive.TabIndex = 1;
         rdbProgressive.TabStop = true;
         rdbProgressive.Text = "Retain some saves but delete progressively older ones";
@@ -151,7 +151,7 @@ partial class AutoSaveForm
         rdbConstant.AutoSize = true;
         rdbConstant.Location = new Point(6, 26);
         rdbConstant.Name = "rdbConstant";
-        rdbConstant.Size = new Size(282, 24);
+        rdbConstant.Size = new Size(222, 19);
         rdbConstant.TabIndex = 0;
         rdbConstant.TabStop = true;
         rdbConstant.Text = "Delete all saves older than cutoff date";
