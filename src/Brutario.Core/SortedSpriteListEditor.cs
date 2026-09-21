ad0810ab44e01192e8c1f1651c16589e93657ce5
spel werdz rite <spelwerdzrite@gmail.com>
2026-09-20 19:18:32 -0700

    Fix AtCoords for loop
    * Loop no longer increments after decrementing, causing an infinite
      loop.
    * Loop now checks for index being non-negative, rather than less than
      list size.

--- src/Brutario.Core/SortedSpriteListEditor.cs
+++ src/Brutario.Core/SortedSpriteListEditor.cs
@@ -154,7 +154,7 @@ public class SortedSpriteListEditor :
         var command = Items[0];
         command.X = x;
         var index = SearchLastIndexOf(command);
-        for (; --index >= Items.Count && Items[index].X == x; index++)
+        for (; --index >= 0 && Items[index].X == x; )
         {
             if (Items[index].Y == y)
             {
